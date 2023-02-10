7a8fea3508cb9049f703ac991c36dd1caa9c3d91
konnylisethalucematorresCAMPUS <lisethtorres969@gmail.com>
2023-02-10 13:26:20 -0500

    solucion ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+
+"""3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+
+A. De izquierda a derecha
+B. De derecha a izquierda"""
+
+print(f"A. De izquierda a derecha")
+print(f"ejemplo","R=20+ 6 – 13 = 13")
